24f20963d73b25ce5d9b72a00528c9469c14ab57
youssefeddahmani-design <youssef.eddahmani@cesjuanpablosegundo.es>
2026-03-25 10:56:04 +0100

    Add files via upload

---
+++ Trabajos_futuro/scrip.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Web cargada correctamente");
+  });
+  
+  function mostrarMensaje() {
+    alert("Formulario enviado 🚀");
+  }
